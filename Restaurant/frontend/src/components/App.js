9b8c7bcf7789ed322c6b1082bccce20f0fb9f90d
Nassir Diallo <nassirougn@gmail.com>
2020-12-22 15:24:51 -0800

    My second commit to set Bootswach, setup the constants file and try some jumbotron and nav-bar using Bootswach

--- Restaurant/frontend/src/components/App.js
+++ Restaurant/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import * as constant from "../constants";
 
 export default class App extends Component {
   constructor(props) {
@@ -7,8 +8,100 @@ export default class App extends Component {
   }
 
   render() {
-    return <p>This is the create room page</p>;
+    return (
+        <React.Fragment>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarColor01"
+          aria-controls="navbarColor01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                {constant.HOME}
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                {constant.FEATURES}
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                {constant.PRICING}
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                {constant.ABOUT}
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                data-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                {constant.DROPDOWN_TEXT}
+              </a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">
+                  {constant.ACTION}
+                </a>
+                <a class="dropdown-item" href="#">
+                  {constant.ANOTHER_SECTION}
+                </a>
+                <a class="dropdown-item" href="#">
+                  {constant.SOMETHING_ELSE_HERE}
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  {constant.SEPARATED_LINK}
+                </a>
+              </div>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input
+              class="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+            />
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">
+              {constant.SEARCH_TEXT}
+            </button>
+          </form>
+        </div>
+      </nav>
+      
+      <div class="jumbotron">
+  <h1 class="display-3">{constant.HELLO_WORLD}</h1>
+  <p class="lead">{constant.JUMBOTRON_TEXT_BODY1}</p>
+  <hr class="my-4" />
+  <p>{constant.JUMBOTRON_TEXT_BODY2}</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">{constant.LEARN_MORE}</a>
+  </p>
+</div>
+</React.Fragment>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ Restaurant/frontend/src/constants/index.js
@@ -0,0 +1,17 @@
+// HEADER CONSTANTS TEXTS
+export const HOME = "Home"
+export const FEATURES = 'Features';
+export const PRICING = 'Pricing';
+export const ABOUT = "About"
+export const ACTION = "Action"
+export const ANOTHER_SECTION = 'Another action';
+export const SOMETHING_ELSE_HERE = 'Something else here';
+export const SEPARATED_LINK = 'Separated link';
+export const SEARCH_TEXT = 'Search';
+export const DROPDOWN_TEXT = 'Dropdown';
+
+// JUMBOTRON CONSTANTS TEXTS
+export const HELLO_WORLD = 'Hello, world!';
+export const JUMBOTRON_TEXT_BODY1 = 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.';
+export const JUMBOTRON_TEXT_BODY2 = 'It uses utility classes for typography and spacing to space content out within the larger container.';
+export const LEARN_MORE = 'Learn more';
